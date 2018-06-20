27f12050c374cd16b49d8d151a4cbc905c0c5f85
lwjfork <lwjfork@gmail.com>
2018-06-20 16:00:05 +0800

    添加自动轮播

--- app/src/main/java/com/lwjfork/test/MainActivity.java
+++ app/src/main/java/com/lwjfork/test/MainActivity.java
@@ -3,21 +3,13 @@ package com.lwjfork.test;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.lwjfork.adapter.adapterview.ArrayListAdapter;
-import com.lwjfork.adapter.adapterview.MultipleAdapter;
-import com.lwjfork.adapter.adapterview.SimpleViewHolder;
-import com.lwjfork.adapter.recycleview.MultipleRecycleAdapter;
-import com.lwjfork.adapter.recycleview.SimpleRecycleViewHolder;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.lwjfork.adapter.viewpager.BannerViewPager;
+import com.lwjfork.adapter.viewpager.SimplePageAdapter;
 
 public class MainActivity extends AppCompatActivity implements IViewHelper, View.OnClickListener {
 
@@ -26,98 +18,19 @@ public class MainActivity extends AppCompatActivity implements IViewHelper, View
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView lv_test = findViewByID(R.id.lv_test);
-        RecyclerView rcv_test = findViewByID(R.id.rcv_test);
-        rcv_test.setVerticalScrollBarEnabled(true);
+        BannerViewPager vp_banner = findViewByID(R.id.vp_banner);
 
-        ArrayListAdapter<String> adapter = new MultipleAdapter<String>().addTypeDelegateItem(new MultipleAdapter.SimpleDelegateItem<String>(android.R.layout.activity_list_item) {
-            @Override
-            public boolean isForType(String item, int position, List<String> data) {
-                return position % 2 == 0;
-            }
+        SimplePageAdapter<TextView> pageAdapter = new SimplePageAdapter<>();
 
-            @Override
-            protected void fillData(String item, SimpleViewHolder viewHolder, int position, List<String> data) {
-                TextView text1 = viewHolder.findViewById(android.R.id.text1);
-                text1.setText(item);
-                text1.setBackgroundColor(getColorById(android.R.color.background_dark));
-            }
-
-        }).addTypeDelegateItem(new MultipleAdapter.SimpleDelegateItem<String>(android.R.layout.activity_list_item) {
-            @Override
-            public boolean isForType(String item, int position, List<String> data) {
-                return position % 3 == 0;
-            }
-
-            @Override
-            protected void fillData(String item, SimpleViewHolder viewHolder, int position, List<String> data) {
-                TextView text1 = viewHolder.findViewById(android.R.id.text1);
-                text1.setText(item);
-                text1.setBackgroundColor(getColorById(R.color.colorPrimaryDark));
-            }
-        }).addTypeDelegateItem(new MultipleAdapter.SimpleDelegateItem<String>(android.R.layout.activity_list_item) {
-            @Override
-            public boolean isForType(String item, int position, List<String> data) {
-                return position % 4 == 0;
-            }
-
-            @Override
-            protected void fillData(String item, SimpleViewHolder viewHolder, int position, List<String> data) {
-                TextView text1 = viewHolder.findViewById(android.R.id.text1);
-                text1.setText(item);
-                text1.setBackgroundColor(getColorById(R.color.colorAccent));
-            }
-        }).addTypeDelegateItem(new MultipleAdapter.SimpleDelegateItem<String>(android.R.layout.activity_list_item) {
-            @Override
-            public boolean isForType(String item, int position, List<String> data) {
-                return true;
-            }
-
-            @Override
-            protected void fillData(String item, SimpleViewHolder viewHolder, int position, List<String> data) {
-                TextView text1 = viewHolder.findViewById(android.R.id.text1);
-                text1.setText(item);
-                text1.setBackgroundColor(getColorById(R.color.colorAccent));
-            }
-        });
-        lv_test.setAdapter(adapter);
-
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 300; i++) {
-            list.add("" + i);
+        for (int i = 0; i < 10; i++) {
+            TextView textView = new TextView(this);
+            textView.setText(i + "");
+            pageAdapter.addItem(textView);
         }
-        adapter.setData(list);
-        MultipleRecycleAdapter<String> recycleAdapter = new MultipleRecycleAdapter<>();
-        recycleAdapter.addTypeDelegateItem(new MultipleRecycleAdapter.SimpleDelegateItem<String>(2, android.R.layout.activity_list_item) {
-            @Override
-            public boolean isForType(List<String> datas, int position) {
-                return position % 2 == 0;
-            }
 
-            @Override
-            public void onBindViewHolder(List<String> datas, SimpleRecycleViewHolder holder, int position) {
-                TextView text1 = holder.findViewById(android.R.id.text1);
-                text1.setText(datas.get(position));
-                text1.setBackgroundColor(getColorById(R.color.colorAccent));
-            }
-        });
-        recycleAdapter.addTypeDelegateItem(new MultipleRecycleAdapter.SimpleDelegateItem<String>(2, android.R.layout.activity_list_item) {
-            @Override
-            public boolean isForType(List<String> datas, int position) {
-                return position % 2 == 1;
-            }
+        vp_banner.setAdapter(pageAdapter);
 
-            @Override
-            public void onBindViewHolder(List<String> datas, SimpleRecycleViewHolder holder, int position) {
-                TextView text1 = holder.findViewById(android.R.id.text1);
-                text1.setText(datas.get(position));
-                text1.setBackgroundColor(getColorById(R.color.colorPrimaryDark));
-            }
-        });
-        rcv_test.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        rcv_test.setAdapter(recycleAdapter);
-        recycleAdapter.setData(list);
-
+        vp_banner.startLoop();
 
     }
 
